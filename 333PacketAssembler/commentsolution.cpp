2945c4252a25f0cb17882102dfc5dfa6efffff7a
Eugene <efoo10@gmail.com>
2018-11-09 17:03:12 +0800

    Create commentsolution.cpp

---
+++ 333PacketAssembler/commentsolution.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <set>
+#include <map>
+
+struct Packet
+{
+	size_t x;
+	size_t y;
+	size_t z;
+	std::string msg;
+
+	Packet() {}
+	Packet(size_t x, size_t y, size_t z, const std::string& msg) : x(x), y(y), z(z), msg(msg) {}
+
+	bool operator<(const Packet& other) const
+	{
+		if (x < other.x)
+			return true;
+		if (x == other.x && y < other.y)
+			return true;
+		return false;
+	}
+	friend std::ostream& operator<<(std::ostream& out, const Packet& p);
+};
+
+std::ostream& operator<<(std::ostream& out, const Packet& p)
+{
+	return out << p.x << "\t" << p.y << "\t" << p.z << "\t" << p.msg;
+}
+
+bool is_complete(const std::set<Packet>& msg)
+{
+	auto front = msg.begin();
+	return msg.size() == front->z;
+}
+
+void print_msg(const std::set<Packet>& msg)
+{
+	for (const auto& packet : msg)
+		std::cout << packet << std::endl;
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::ifstream testFile;
+	testFile.open("test.txt");
+
+	std::string line;
+	std::map<int, std::set<Packet>> messages;
+	while (getline(testFile, line))
+	{
+		std::stringstream ss(line);
+		Packet p;
+		ss >> p.x >> p.y >> p.z;
+		getline(ss, p.msg);
+		messages[p.x].insert(p);
+		auto msg = messages.find(p.x);
+		if (is_complete(msg->second))
+		{
+			print_msg(msg->second);
+			messages.erase(msg);
+		}
+	}
+
+	std::cin.get();
+	std::cin.ignore();
+}
